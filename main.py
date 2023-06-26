e1819f5dc013b5560ba4664721c961e891b8632a
damjanh <damjan.hudboreznik@gmail.com>
2023-06-26 13:11:12 +0200

    Fix fruit respawn on snake body.

--- main.py
+++ main.py
@@ -135,6 +135,10 @@ class Main:
             self.snake.add_block()
             self.snake.play_eat_sound()
 
+        for block in self.snake.body[1:]:
+            if block == self.fruit.pos:
+                self.fruit.randomize()
+
     def check_fail(self):
         if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
             self.game_over()
